527774a3c2919b7b6d929b6526600bcfbd15ec5d
Igor Farias <igor.atf@gmail.com>
2025-08-20 17:44:05 -0300

    add analytics

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import { Navbar } from "./components/Navbar";
 import { HomePage } from "./pages/HomePage";
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   return (
     <main>
       <Navbar />
       <HomePage />
+      <Analytics />
     </main>
   );
 }
