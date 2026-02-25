29d9a0ee0895847fa5995c524a7ac08ae4410f49
Igor Farias <igor.atf@gmail.com>
2026-02-25 15:12:09 -0300

    duplicate types and start working on functions to calculate score

--- backend/controllers/match.controller.ts
+++ backend/controllers/match.controller.ts
@@ -1,6 +1,24 @@
 import { type Request, type Response } from "express";
+import type { KingQueenResourceName, Player } from "../types.js";
 
 export const saveMatch = (req: Request, res: Response) => {
-  console.log("req ", req.body);
+  calculateKingsAndQueens(req.body.players);
   res.json({ data: "This is a test" });
+};
+
+export const calculateKingsAndQueens = (players: Player[]) => {
+  return players.forEach((player) => calculateContrabandBonus(player));
+};
+
+const calculateContrabandBonus = (player: Player) => {
+  const playerContrabands = player.contrabands;
+  console.log("PREVIOUS ", player);
+  playerContrabands.map((playerContraband) => {
+    const { resourceType, resourceBonus } = playerContraband;
+    if (resourceType && resourceBonus && resourceType in player) {
+      player[resourceType as KingQueenResourceName] +=
+        resourceBonus * playerContraband.quantity;
+    }
+  });
+  console.log("AFTER BONUS ", player);
 };
---
+++ backend/types.d.ts
@@ -0,0 +1,48 @@
+export type ResourceName =
+  | "apple"
+  | "bread"
+  | "cheese"
+  | "chicken"
+  | "coin"
+  | "contraband";
+
+export type KingQueenResourceName = Exclude<
+  ResourceName,
+  "coin" | "contraband"
+>;
+
+export interface Contraband {
+  name: string;
+  score: number;
+  resourceBonus?: number;
+  resourceType?: ResourceName;
+}
+
+export interface PlayerContraband extends Contraband {
+  quantity: number;
+}
+
+export interface Player {
+  name: string;
+  apple: number;
+  bread: number;
+  cheese: number;
+  chicken: number;
+  contrabands: PlayerContraband[];
+  coin: number;
+}
+
+export interface PlayerScore {
+  apple: number;
+  bread: number;
+  cheese: number;
+  chicken: number;
+  coin: number;
+  contraband: number;
+  total: number;
+}
+
+export interface KingsAndQueens {
+  kings: Record<KingQueenResourceName, Player[]>;
+  queens: Record<KingQueenResourceName, Player[]>;
+}
