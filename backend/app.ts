c2fd8d5463525faa79ad1df25dd5d74a0e75b87b
Igor Farias <igor.atf@gmail.com>
2026-04-08 16:57:16 -0300

    add tanstack query and cors packages

--- backend/app.ts
+++ backend/app.ts
@@ -2,14 +2,15 @@ import express from "express";
 import { PORT } from "./config/env.js";
 import { matchRouter } from "./routes/match.routes.js";
 import { errorMiddleware } from "./middlewares/errors.middleware.js";
+import cors from "cors";
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use("/v1/match", matchRouter);
 
-app.get("/", (req, res) => {
-  console.log("works");
+app.get("/", (_req, res) => {
   res.send("API is running");
 });
 
